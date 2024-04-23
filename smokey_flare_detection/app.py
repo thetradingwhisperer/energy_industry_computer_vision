0371b56f36de4253eccb6343cb6f02cd17de729a
The Trading Whisperer <29692338+thetradingwhisperer@users.noreply.github.com>
2024-04-23 12:53:19 +0000

    Createda streamlit app

--- smokey_flare_detection/app.py
+++ smokey_flare_detection/app.py
@@ -2,33 +2,53 @@ import streamlit as st
 import cv2
 import numpy as np
 from PIL import Image
+from utils import make_inference
+import tempfile
 
 def load_image(image_file):
     img = Image.open(image_file)
-    return img
+    img_for_model = np.array(img)
+    img_for_model_bgr = cv2.cvtColor(img_for_model, cv2.COLOR_RGB2BGR)
 
-st.title('Smoke Detection Demo')
-st.write("This demo shows how our smoke detection model works on the offshore platforms.")
+    # Save the numpy array image to a temporary file
+    temp = tempfile.NamedTemporaryFile(delete=False, suffix='.jpg')
+    cv2.imwrite(temp.name, img_for_model_bgr)
 
-image_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
-if image_file is not null:
-    image = load_image(image_file)
-    st.image(image, caption='Uploaded Image', use_column_width=True)
-    if st.button('Detect Smoke'):
-        # Assuming 'detect_smoke' is a function that processes the image and returns an annotated one
-        processed_image = detect_smoke(image)
-        st.image(processed_image, caption='Processed Image with Detected Smoke', use_column_width=True)
+    return img, temp.name
 
-# Model Metrics
-st.header("Model Metrics")
-st.write("Here you can see the performance metrics of our smoke detection model.")
-# Assuming metrics are stored in a dictionary
-metrics = {'Accuracy': 0.95, 'Precision': 0.88, 'Recall': 0.90}
-for metric, value in metrics.items():
-    st.metric(label=metric, value=f"{value * 100}%")
 
+st.title('Smokey Flare Detection Demo')
+st.write("This demo shows how our smoke detection model works on the offshore flaring.")
 # Documentation
 st.header("How It Works")
 st.write("""
-Our smoke detection model is trained on thousands of images from oil platforms, using a deep learning framework to identify smoke patterns even in challenging conditions. By implementing this model, we aim to enhance safety measures and response times significantly.
+Our smoke detection model is trained on thousands of images from oil platforms, 
+using a deep learning framework to identify smoke patterns even in challenging conditions. 
+By implementing this model, we aim to use AI to identify smokey flaring situations and consequently
+trigger investigation to mitigate these events.
 """)
+st.divider()
+st.write("To use the model, upload an image of an offshore platform and click the 'Detect' button.")
+
+image_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+if image_file is not None:
+    image, image_path = load_image(image_file)
+    #Define 2 columns
+    col1, col2 = st.columns(2)
+
+    with col1:
+        col1.header('Original Image')
+        #st.image(image, caption='Uploaded Image', use_column_width=True)
+        st.image(image_path, caption='Uploaded Image', use_column_width=True)
+        if st.button('Detect'):
+            with col2:
+                # Assuming 'detect_smoke' is a function that processes the image and returns an annotated one
+                col2.header('Processed Image')
+                processed_image = make_inference(image_path)
+                processed_image = cv2.cvtColor(processed_image, cv2.COLOR_BGR2RGB)
+                st.image(processed_image, 
+                    caption='Processed Image with Detected Smoke', use_column_width=True)
+
+
+
+
---
+++ smokey_flare_detection/utils.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from roboflow import Roboflow
+import supervision as sv
+import cv2
+import time
+
+
+
+def make_inference(image_path):
+
+    #Load model
+    rf = Roboflow(api_key="eytDtQ1Q75OZyEFEgHNF")
+    project = rf.workspace().project("fmmk2")
+    model = project.version(1).model
+
+    result = model.predict(image_path, confidence=40).json()
+    image = cv2.imread(image_path)
+
+    #Making inference
+    labels = [item["class"] for item in result["predictions"]]
+    print('Making detection on the image...')
+    time.sleep(1)
+    print(f'- Detection 1: {labels[0]} has been detected in the image')
+    try:
+        print(f'- Detection 2: {labels[1]} has also been detected in the image')
+    except:
+        pass 
+    detections = sv.Detections.from_inference(result)
+
+    #Defining the annotator on the image or video
+    label_annotator = sv.LabelAnnotator(text_thickness=0, text_position=sv.Position.BOTTOM_RIGHT)
+    main_annotator =   sv.ColorAnnotator(opacity=0.3) #sv.MaskAnnotator()
+
+    annotated_image = main_annotator.annotate(scene=image, 
+                                              detections=detections)
+    annotated_image = label_annotator.annotate(scene=annotated_image,
+                                                detections=detections, 
+                                                labels=labels)
+
+    sv.plot_image(image=annotated_image, size=(10, 7))
+    return annotated_image
+
+if __name__ == "__main__":
+    main()
