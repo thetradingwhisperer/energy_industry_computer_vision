263946559b26dcd6ebf7ad53521528396247d433
The Trading Whisperer <29692338+thetradingwhisperer@users.noreply.github.com>
2024-06-04 11:31:32 +0000

    Added a new model

--- smokey_flare_detection/app.py
+++ smokey_flare_detection/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import cv2
 import numpy as np
 from PIL import Image
-from utils import make_inference
+from utils import make_inference_normal, make_inference_ringelmaan
 import tempfile
 
 def load_image(image_file):
@@ -21,6 +21,10 @@ with st.sidebar:
     st.write('Please upload an image of an offshore platform to detect smoke.')
     st.write('The model will identify smoke patterns in the image.')
     st.write('Click the "Detect" button to see the results.')
+    st.divider()
+
+    confidence_threshold = st.slider('Set detection confidence Threshold', 0, 100, 40)
+
 
     st.header("How It Works")
     st.markdown("""
@@ -33,31 +37,57 @@ st.title('Smokey Flare Detection with AI')
 st.write("This demo shows how our smoke detection model works on the offshore flaring.")
 # Documentation
 st.divider()
-
-
 image_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
-confidence_threshold = st.slider('Set detection confidence Threshold', 0, 100, 40)
-if image_file is not None:
-    image, image_path = load_image(image_file)
-    #Define 2 columns
-    col1, col2 = st.columns(2)
 
-    with col1:
-        col1.markdown('### Original Image')
-        #st.image(image, caption='Uploaded Image', use_column_width=True)
-        st.image(image_path, caption='Uploaded Image', use_column_width=True)
-        #add empty space
-        st.write('')
-        if st.button('Detect'):
-            with col2:
-                # Assuming 'detect_smoke' is a function that processes the image and returns an annotated one
-                col2.markdown('### AI Processed Image')
-                processed_image, labels = make_inference(image_path, confidence=confidence_threshold)
-                processed_image = cv2.cvtColor(processed_image, cv2.COLOR_BGR2RGB)
-                st.image(processed_image, 
-                    caption='Processed Image with Detected Smoke', use_column_width=True)
-                st.html(f'<p style="color:red;"> Results: {labels} detected in the image</p>')
+# Add two tabs for two prediction type: 1) fire and some 2) fire and different type of smoke
+tab1, tab2 = st.tabs(["Normal", "Ringelmaan"])
+tab1.subheader("Fire and Smoke Detection")
+tab2.subheader("Fire and Ringelmaan Smoke Type Detection")
 
+with tab1:
+    st.write("This tab shows the detection of fire and smoke in the image.")
+    if image_file is not None:
+        image, image_path = load_image(image_file)
+        #Define 2 columns
+        col1, col2 = st.columns(2)
 
+        with col1:
+            col1.markdown('###### Original Image')
+            #st.image(image, caption='Uploaded Image', use_column_width=True)
+            st.image(image_path, caption='Uploaded Image', use_column_width=True)
+            #add empty space
+            st.write('')
+            if st.button('Detect', key='detect_normal'):
+                with col2:
+                    # Assuming 'detect_smoke' is a function that processes the image and returns an annotated one
+                    col2.markdown('###### AI Processed Image')
+                    processed_image, labels = make_inference_normal(image_path, confidence=confidence_threshold)
+                    processed_image = cv2.cvtColor(processed_image, cv2.COLOR_BGR2RGB)
+                    st.image(processed_image, 
+                        caption='Processed Image with Detected Smoke', use_column_width=True)
+                    st.html(f'<p style="color:red;"> Results: {labels} detected in the image</p>')
+
+with tab2:
+    st.write("This tab shows the detection of fire and different smoke type according to ringelmaan in the image.")
+    if image_file is not None:
+        image, image_path = load_image(image_file)
+        #Define 2 columns
+        col1_2, col2_2 = st.columns(2)
+
+        with col1_2:
+            col1.markdown('###### Original Image')
+            #st.image(image, caption='Uploaded Image', use_column_width=True)
+            st.image(image_path, caption='Uploaded Image', use_column_width=True)
+            #add empty space
+            st.write('')
+            if st.button('Detect', key='detect_ringelmaan'):
+                with col2_2:
+                    # Assuming 'detect_smoke' is a function that processes the image and returns an annotated one
+                    col2.markdown('###### AI Processed Image')
+                    processed_image, labels = make_inference_ringelmaan(image_path, confidence=confidence_threshold)
+                    processed_image = cv2.cvtColor(processed_image, cv2.COLOR_BGR2RGB)
+                    st.image(processed_image, 
+                        caption='Processed Image with Detected Smoke', use_column_width=True)
+                    st.html(f'<p style="color:red;"> Results: {labels} detected in the image</p>')
 
 
--- smokey_flare_detection/utils.py
+++ smokey_flare_detection/utils.py
@@ -6,7 +6,7 @@ import time
 
 
 
-def make_inference(image_path, confidence=40):
+def make_inference_normal(image_path, confidence=40):
 
     #Load model
     rf = Roboflow(api_key="eytDtQ1Q75OZyEFEgHNF")
@@ -40,5 +40,37 @@ def make_inference(image_path, confidence=40):
     sv.plot_image(image=annotated_image, size=(10, 7))
     return annotated_image, labels
 
-if __name__ == "__main__":
-    main()
+
+def make_inference_ringelmaan(image_path, confidence=40):
+
+    #Load model
+    rf = Roboflow(api_key="eytDtQ1Q75OZyEFEgHNF")
+    project = rf.workspace().project("smokey-flaring-detection")
+    model = project.version(1).model
+
+    result = model.predict(image_path, confidence=confidence).json()
+    image = cv2.imread(image_path)
+
+    #Making inference
+    labels = [item["class"] for item in result["predictions"]]
+    print('Making detection on the image...')
+    time.sleep(1)
+    print(f'- Detection 1: {labels[0]} has been detected in the image')
+    try:
+        print(f'- Detection 2: {labels[1]} has also been detected in the image')
+    except:
+        pass 
+    detections = sv.Detections.from_inference(result)
+
+    #Defining the annotator on the image or video
+    label_annotator = sv.LabelAnnotator(text_thickness=0, text_position=sv.Position.BOTTOM_LEFT)
+    main_annotator =   sv.ColorAnnotator(opacity=0.3) #sv.MaskAnnotator()
+
+    annotated_image = main_annotator.annotate(scene=image, 
+                                              detections=detections)
+    annotated_image = label_annotator.annotate(scene=annotated_image,
+                                                detections=detections, 
+                                                labels=labels)
+
+    sv.plot_image(image=annotated_image, size=(10, 7))
+    return annotated_image, labels
