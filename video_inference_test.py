f849c2cab7cde881efc193f8ba11c6f396062c7c
The Trading Whisperer <29692338+thetradingwhisperer@users.noreply.github.com>
2024-09-16 10:44:11 +0000

    another video

--- video_inference_test.py
+++ video_inference_test.py
@@ -1,11 +1,11 @@
 from inference import InferencePipeline
 from inference.core.interfaces.stream.sinks import VideoFileSink
 
-var = 1
+var = 6
 video_sink = VideoFileSink.init(video_file_name=f"output{var}.avi")
 pipeline = InferencePipeline.init(
         model_id="smokey-flaring-detection/2",
-        video_reference=f"./videos/NOC-flare1.mp4",
+        video_reference=f"./videos/Flaring-2-real.mp4",
         on_prediction=video_sink.on_prediction,
         api_key="eytDtQ1Q75OZyEFEgHNF"
     )
